fd141047ddb3333c5f314806efe2d5074139381d
0b1kn00b <polysemantic@gmail.com>
2021-08-06 12:39:01 +0200

    updated updater

--- update.sh
+++ update.sh
@@ -1,3 +1,8 @@
 #!/bin/bash 
 
+git config --global status.submoduleSummary true
+git pull 
+git submodule update --init --remote
+git submodule foreach -q --recursive 'git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
+
 git pull --recurse-submodules
